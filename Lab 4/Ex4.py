9eba226906f04e8a82ee9266be4b7606cbd2a15d
Noah Zane <noahza@hawaii.edu>
2026-09-16 19:07:40 -1000

    Added and edited Ex 2 to 4

--- Lab 4/Ex4.py
+++ Lab 4/Ex4.py
@@ -3,7 +3,7 @@
 #Date: Sept 16, 2026
 
 survey_respondents = (1012, 1035, 1021, 1053)
-survey_respondents.append(1067) #This will raise an AttributeError because tuples are immutable
+#survey_respondents.append(1054)  # This will raise an AttributeError because tuples are immutable
 
-survey_respondents = survey_respondents + (1054,)
-print("Updated Survey Respondents:", survey_respondents)
+survey_respondents = survey_respondents + (1054,)  # This creates a new tuple with the additional respondent
+print("Updated survey respondents:", survey_respondents)
